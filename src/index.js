533e23c8af6d15a9d9fa732086739019aef00843
Dominick Siciliano <brotherhood1511@gmail.com>
2026-05-06 17:35:33 -0400

    Lab finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
- const form = document.getElementById('create-task-form')
+  const form = document.getElementById('create-task-form')
 
  form.addEventListener('submit', (event) => {
   event.preventDefault()
