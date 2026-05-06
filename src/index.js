0749b852f0308f08bcc12338316956e65e324ec4
Dominick Siciliano <brotherhood1511@gmail.com>
2026-05-06 17:33:45 -0400

    Lab finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+ const form = document.getElementById('create-task-form')
+
+ form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const input = document.getElementById('new-task-description')
+  const task = input.value 
+
+  buildToDo(task)
+
+  input.value = ''
+ })
+})
+
+function buildToDo(task) {
+  const ul = document.getElementById('tasks')
+
+  const li = document.createElement('li')
+  li.textContent = task
+
+  ul.appendChild(li)
+ }
